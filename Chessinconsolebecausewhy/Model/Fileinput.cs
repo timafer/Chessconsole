473a46a515f3c088c8b342c7836700a1b80e7db5
timafer <boomer3000@comcast.net>
2016-07-28 09:32:16 -0600

    Single move working

--- Chessinconsolebecausewhy/Model/Fileinput.cs
+++ Chessinconsolebecausewhy/Model/Fileinput.cs
@@ -11,6 +11,8 @@ namespace Chessinconsolebecausewhy.Model
     {
         string filepath = "";
         List<string> lines = new List<string>();
+        const int asciibal = 65;
+        const int firstbal = 48;
         Model.BoardSquare[,] Board;
         public Fileinput(string path, Model.BoardSquare[,] board)
         {
@@ -45,11 +47,14 @@ namespace Chessinconsolebecausewhy.Model
                 {
                     string piece = processplace(s);
                     Console.WriteLine("-place the "+piece+" on "+s[2]+s[3]);
-                    Board[(char.ToUpper(s[2]) - 64), char.ToUpper(s[3])-48].contains = piece;
+                    int t1 = char.ToUpper(s[2]) - asciibal;
+                    int t2 = char.ToUpper(s[3]) - firstbal;
+                    Console.WriteLine(""+t1+t2);
+                    Board[(char.ToUpper(s[2]) - asciibal), char.ToUpper(s[3])-firstbal].contains = piece;
                 }
                 else if (result.Length==2)
                 {
-                    Console.WriteLine("-moved the "+Board[char.ToUpper(result[0][0])-65, char.ToUpper(result[0][1])-48].contains +" at "+result[0][0]+result[0][1]+" to " + result[1][0] + result[1][1]);
+                    Console.WriteLine(prossesmove(result));
                 }
                 else if (result.Length == 4)
                 {
@@ -103,6 +108,47 @@ namespace Chessinconsolebecausewhy.Model
                     break;
             }
             return result;
+        }
+        public string prossesmove(string[] result)
+        {
+            string print;
+            if (result[0].Length==2)
+            {
+                if (Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains.Equals("Empty"))
+                {
+                    print = ("-Tried to move piece at " + result[0][0] + result[0][1] + " to " + result[1][0] + result[1][1] + " but it was empty");
+                }
+                else
+                {
+                    print = ("-moved the " + Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains + " at " + result[0][0] + result[0][1] + " to " + result[1][0] + result[1][1]);
+                    Board[char.ToUpper(result[1][0]) - asciibal, char.ToUpper(result[1][1]) - firstbal].contains = Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains;
+                    Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains="Empty";
+                }
+            }           
+            else if (result[0][2].Equals('*'))
+            {
+                if (Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains.Equals("Empty"))
+                {
+                    print = ("-Tried to move piece at " + result[0][0] + result[0][1] + " to take piece at " + result[1][0] + result[1][1] + " but it was empty");
+                }
+                else if (Board[char.ToUpper(result[1][0]) - 65, char.ToUpper(result[1][1]) - 48].contains.Equals("Empty"))
+                {
+                    print = ("-moved the " + Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains + " at " + result[0][0] + result[0][1] + " to try to take the piece at" + result[1][0] + result[1][1] + "But it was empty");
+                    Board[char.ToUpper(result[1][0]) - asciibal, char.ToUpper(result[1][1]) - firstbal].contains = Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains;
+                    Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains = "Empty";
+                }
+                else
+                {
+                    print = ("-moved the " + Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains + " at " + result[0][0] + result[0][1] + " to " + result[1][0] + result[1][1]+" and took the "+ Board[char.ToUpper(result[1][0]) - asciibal, char.ToUpper(result[1][1]) - firstbal].contains);
+                    Board[char.ToUpper(result[1][0]) - asciibal, char.ToUpper(result[1][1]) - firstbal].contains = Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains;
+                    Board[char.ToUpper(result[0][0]) - asciibal, char.ToUpper(result[0][1]) - firstbal].contains = "Empty";
+                }
+            }
+            else
+            {
+                print=("Invalid 3 charater on movement");
+            }
+            return print;
         }
     }
 }
